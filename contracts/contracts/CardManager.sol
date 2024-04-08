8cd46127b9d5e8abdbfdf1a1bf63a67e9bbb5ec4
Dante Luis <dsldante@gmail.com>
2024-04-08 13:57:45 -0400

    Started adding VRF packages and setup

---
+++ contracts/contracts/CardManager.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Apache
+pragma solidity ^0.8.25;
+
+import {VRFCoordinatorV2Interface} from "smartcontractkit/chainlink@2.5.0/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "smartcontractkit/chainlink@2.5.0/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {ConfirmedOwner} from "smartcontractkit/chainlink@2.5.0/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+
+contract CardManager {
+  constructor() {
+    
+  }
+}
