a4591e473718d26ed7e751211014615bc5cbdecd
Dante Luis <dsldante@gmail.com>
2024-04-16 14:13:38 -0400

    Added manager for character tokens

--- contracts/contracts/CardManager.sol
+++ contracts/contracts/CardManager.sol
@@ -5,6 +5,40 @@ import {VRFCoordinatorV2Interface} from "smartcontractkit/chainlink@2.9.0/contra
 import {VRFConsumerBaseV2} from "smartcontractkit/chainlink@2.9.0/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import {ConfirmedOwner} from "smartcontractkit/chainlink@2.9.0/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 
+contract AICharacterGenerator is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    // Chainlink Node address
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    // store data from API
+    string public characters;
+
+    // event for API result
+    event RequestCharactersFulfilled(bytes32 indexed requestId, string indexed data);
+
+    constructor() {
+        setPublicChainlinkToken();
+        oracle = 0x<Oracle_Address>;
+        jobId = "<Job_ID>";
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
+
+    function requestCharacterData(string memory apiUrl) public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        request.add("get", apiUrl);
+        request.add("path", "data.path.to.characters"); // adjest to match the structure
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfill(bytes32 _requestId, string memory _characters) public recordChainlinkFulfillment(_requestId) {
+        characters = _characters;
+        emit RequestCharactersFulfilled(_requestId, _characters);
+    }
+}
+
 contract CardManager is VRFConsumerBaseV2, ConfirmedOwner {
   event RequestSent(uint256 requestId, uint32 numWords);
     event RequestFulfilled(uint256 requestId, uint256[] randomWords);
