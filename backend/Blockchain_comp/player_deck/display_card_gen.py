14f375b720655330076d3d4a38c94c3cdd12597a
mickannor <89411564+mickannor@users.noreply.github.com>
2024-04-19 12:46:03 -0400

    display card

--- backend/Blockchain_comp/player_deck/display_card_gen.py
+++ backend/Blockchain_comp/player_deck/display_card_gen.py
@@ -3,11 +3,13 @@
 # from ai import starter_pack
 from easy_pil import Editor, Font
 import textwrap
+import os
 
+base = os.getcwd() + '/Blockchain_comp/player_deck/assets/'
 
-main = Font("/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/Vecna.otf", size=50)
-desc =  Font("/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/Vecna.otf", size=35)
-stats = Font("/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/Vecna Bold.otf", size=35)
+main = Font(f'{base}Vecna.otf', size=50)
+desc =  Font(f'{base}Vecna.otf', size=35)
+stats = Font(f'{base}Vecna Bold.otf', size=35)
 # character = starter_pack[0]
 character = {
     "Name": "Aurora Silverleaf",
@@ -24,13 +26,15 @@ character = {
     "Backstory": "Aurora was trained in the arcane arts by a powerful wizard, learning to harness the forces of magic to shape reality to her will. She now seeks out ancient secrets and forbidden knowledge, using her powers to unlock the mysteries of the universe.",
     "Equipment": " Robes, spellbook, arcane focus, components pouch\nWeapon: None"
 }
-background = Editor("/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/card_bg.png")
-pfp = Editor("/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/pfp.png").resize((804,595))
+background = Editor(f'{base}card_bg.png')
+pfp = Editor(f'{base}pfp.png').resize((804,595))
 background.paste(pfp.image, (83,158))
 # square = Canvas((300, 300))
 # square = Editor(canvas=square)
 
-Class = Editor(f'/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/{(character["Class"]).lower().strip(" ")}.png').resize((100,100))
+# Class = Editor(f'/home/mick/Documents/S24_Chain_Cards/backend/testing/truffle/assets/{(character["Class"]).lower().strip(" ")}.png').resize((100,100))
+Class = Editor(f'{base}{(character["Class"]).lower().strip(" ")}.png').resize((100,100))
+# Class = Editor(f'{(character["Class"]).lower().strip(' ')}.png').resize((100, 100))
 background.paste(Class.image, (786,847))
 background.text((80, 90), character["Name"], font=main, color="black")
 background.text((80, 780), character["Race"], font=main, color="black")
